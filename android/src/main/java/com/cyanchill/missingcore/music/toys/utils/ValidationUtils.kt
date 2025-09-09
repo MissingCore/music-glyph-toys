c7166a1634a5e78bfdd16beaf6cfec2cff4065e9
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-09-08 21:53:27 -0400

    chore: Helper to determine if we shouldn't run the code
    - Currently, we should only run this if the device is Android 15+ and is a Nothing Phone 3.

---
+++ android/src/main/java/com/cyanchill/missingcore/music/toys/utils/ValidationUtils.kt
@@ -0,0 +1,15 @@
+package com.cyanchill.missingcore.music.toys.utils
+
+import android.os.Build
+
+object ValidationUtils {
+  /** Helps ensure we run this code on supported devices. */
+  fun isDeviceSupported(): Boolean {
+    val isAndroid35OrHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM
+
+    val isNothingManufacturer = Build.MANUFACTURER.equals("Nothing", ignoreCase = true)
+    val isNothingPhone3 = Build.MODEL.equals("A024", ignoreCase = true)
+
+    return isAndroid35OrHigher && isNothingManufacturer && isNothingPhone3
+  }
+}
