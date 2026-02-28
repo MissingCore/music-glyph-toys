c24b8f6a97e1ad2d13a038a31b3474a6eb46e703
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-02-28 14:47:16 -0500

    docs: Add notes about exported functions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import * as GlyphButton from './GlyphButton';
 import MusicGlyphToys from './MusicGlyphToys';
 
+/** Create a connection to the Glyph Matrix in order for `setMatrixArtwork` to work. */
 export function setUpToy() {
   return MusicGlyphToys.setUpToy();
 }
 
-export function getDeviceInfo() {
-  return MusicGlyphToys.getDeviceInfo();
+/**
+ * Get the Glyph Matrix to display the artwork specified by the uri.
+ * @deprecated Currently unimplemented.
+ */
+export async function setMatrixArtwork(uri: string) {
+  return MusicGlyphToys.setMatrixArtwork(uri);
 }
 
-export function setMatrixArtwork(uri: string) {
-  return MusicGlyphToys.setMatrixArtwork(uri);
+/** @deprecated Primarily for debugging. */
+export function getDeviceInfo() {
+  return MusicGlyphToys.getDeviceInfo();
 }
 
 export { GlyphButton };
