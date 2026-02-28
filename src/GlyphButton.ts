2b50933ab5cde4a774729305861cc2261b2dc642
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-02-28 15:13:12 -0500

    chore: Mark some events as `@deprecated` as they're unused

--- src/GlyphButton.ts
+++ src/GlyphButton.ts
@@ -2,8 +2,11 @@ import MusicGlyphToys from './MusicGlyphToys';
 
 //#region Events
 export const onMount = MusicGlyphToys.onMount;
+/** @deprecated Currently unused. */
 export const onShortPress = MusicGlyphToys.onShortPress;
+/** @deprecated Currently unused. */
 export const onLongPress = MusicGlyphToys.onLongPress;
+/** @deprecated Currently unused. */
 export const onTouchDown = MusicGlyphToys.onTouchDown;
 export const onTouchUp = MusicGlyphToys.onTouchUp;
 //#endregion
