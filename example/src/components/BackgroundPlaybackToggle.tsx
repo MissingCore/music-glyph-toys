731c3241a93b7a797f99a40a19689f81402cb85a
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-02-28 15:54:08 -0500

    chore: Add switch so that the user doesn't need to force-stop the app to stop playback from continuing after the app is dismissed

---
+++ example/src/components/BackgroundPlaybackToggle.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useState } from 'react';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+
+import { setPlayerOptions } from '../utils/react-native-track-player';
+
+export function BackgroundPlaybackToggle() {
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const onChange = useCallback((value: boolean) => {
+    setPlayerOptions(value);
+    setIsEnabled(value);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Continue Playback on Dismiss:</Text>
+      <Switch value={isEnabled} onValueChange={onChange} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 8,
+    padding: 16,
+  },
+  text: {
+    fontSize: 14,
+  },
+});
